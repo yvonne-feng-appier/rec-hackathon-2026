a3c3f49397d357d8de8cc0f8cdb1a79074c4b36b
Will Chen <will.wc.chen@appier.com>
2026-02-12 18:35:43 +0800

    fix: use proxy

--- scripts/manual_test_all.sh
+++ scripts/manual_test_all.sh
@@ -2,13 +2,12 @@
 set -e
 
 # Run tests
-run_test inl_corp_1 650alldb2 300 300
-run_test inl_corp_1 dynamicntdw1 1200 627
+./scripts/manual_test.sh inl_corp_1 650alldb2 300 300
+./scripts/manual_test.sh inl_corp_1 dynamicntdw1 1200 627
 
-run_test inl_corp_2 650alldb2 300 300
-run_test inl_corp_2 dynamicntdw1 1200 627
+./scripts/manual_test.sh inl_corp_2 650alldb2 300 300
+./scripts/manual_test.sh inl_corp_2 dynamicntdw1 1200 627
 
-# Keeta doesn't require proxy
 ./scripts/manual_test.sh keeta "" 300 300
 
 echo ""
