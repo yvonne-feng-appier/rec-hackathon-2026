c30ebe98085de45e2d49a1c73ba7314791540d10
yvonne-feng-appier <203782860+yvonne-feng-appier@users.noreply.github.com>
2026-02-12 09:00:56 +0000

    chore(vendors): add or update vendor from issue

--- internal/strategy/header/adpopcorn_header_test.go
+++ internal/strategy/header/adpopcorn_header_test.go
@@ -9,5 +9,8 @@ import (
 func TestAdPopcornHeader(t *testing.T) {
 	h := &AdpopcornHeader{UserAgent: "tzyu.net", ContentType: "application/json"}
 	headers := h.GenerateHeaders(Params{})
-	assert.Equal(t, map[string]string{"User-Agent": "tzyu.net", "Content-Type": "application/json"}, headers)
+	assert.Equal(t, map[string]string{
+		"User-Agent":   "tzyu.net",
+		"Content-Type": "application/json",
+	}, headers)
 }
